910800737e18c0a04e5f21a5ff44fa9d23a699ff
root <root@89-111-137-34.swtest.ru>
2026-08-06 15:01:32 +0300

    Автоматическая синхронизация публичных файлов 2026-08-06 15:01:32

--- webapp/sw.js
+++ webapp/sw.js
@@ -1,19 +1,34 @@
+const CACHE_VERSION = 'v2';  // Увеличивайте при каждом изменении кешируемых файлов
+
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('v1').then(cache => {
+        caches.open(CACHE_VERSION).then(cache => {
             return cache.addAll([
                 '/pwa',
                 '/manifest.json',
                 '/webapp/categories.js',
-                '/fonts/Roboto-Regular.woff2',
-                '/fonts/Roboto-Bold.woff2',
                 '/picture/LOGO.jpg',
                 '/webapp/index.html',
-                '/webapp/max.html'
+                '/webapp/max.html',
+                '/webapp/pwa.html',
+                '/icons/apple-icon-180x180.png',
+                '/icons/icon-192.png',
+                '/icons/icon-512.png'
             ]);
         })
     );
 });
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_VERSION).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
